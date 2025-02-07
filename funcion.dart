765f62cc8b1648de8d50292350dc2c1e259d843d
Uziel-Perez1090 <167554764+Uziel-Perez1090@users.noreply.github.com>
2025-02-07 16:49:42 +0000

    mi primera vez

---
+++ funcion.dart
@@ -0,0 +1,18 @@
+// funcion que no devuelve valor
+void saludar(){
+  print('hola mundo');
+}
+
+void escribeNombre(){
+  print('Uziel Perez Morales');
+}
+
+void main(){
+  // Uziel Perez Morales Mat: 22308051281090
+  // Llamar a la funcion saludar
+  print('Uziel Perez Morales Mat: 22308051281090');
+  print('Llamando a la funcion saludar');
+  saludar();
+  print('Llamando a la funcion escribeNombre');
+  escribeNombre(); 
+}
