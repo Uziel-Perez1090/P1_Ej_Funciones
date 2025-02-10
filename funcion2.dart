35f1bfe662633568eac5b0d5d953fc20aabfd725
Uziel-Perez1090 <167554764+Uziel-Perez1090@users.noreply.github.com>
2025-02-10 18:04:25 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,14 @@
+//Funciones creadas por el usuario
+void suma(int a, int b) {
+  print('la suma es: ${a + b}');
+}
+void resta(int a, int b) {
+  print('la resta es: ${a - b}');
+}
+//Funcion Main
+void main(){
+  print('Uziel Perez Morales Mat: 22308051281090');
+  print('llamando a la funcion suma y resta');
+  suma(10, 5);
+  resta(10, 5);
+}
